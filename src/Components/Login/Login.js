e4eef252cbf81aa7afe7c10d37e3fe02e551b750
transcending92 <melaniekealacampbell@gmail.com>
2022-07-21 20:19:11 -0500

    Login Page
    Added line breaks in between the username, password, login, and forgot password entries

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -6,9 +6,13 @@ const Login = () => {
     <div className="container">
         <label>Username</label>
             <input type="text" placeholder="Enter Username" name="username" required />
+            <br />
         <label>Password</label>
             <input type="text" placeholder="Enter Password" name="password" required />
+            <br />
         <button type="submit">Log in</button>
+        <br />
+        Forgot <a href="#"> password? </a>   
     </div>
    </form>
   )

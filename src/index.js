efa6bc940009bba74f42801d29d876627cadb7a0
transcending92 <melaniekealacampbell@gmail.com>
2022-07-24 00:10:38 -0500

    Fixed bugs
    Working on getting the code to display on the screen

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.scss";
 import App from "./components/App";
+import {BrowserRouter} from 'react-router-dom';e
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
 
 ReactDOM.render(
   <React.StrictMode>
+<BrowserRouter>
     <App />
+</BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
-

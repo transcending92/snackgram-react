f59610ff6dae4c4193cb9e6158234be13bdbc573
transcending92 <melaniekealacampbell@gmail.com>
2022-07-25 13:55:44 -0500

    Working on CSS
    Can not see the page in order to update the css as needed

--- src/index.js
+++ src/index.js
@@ -2,20 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.scss";
 import App from "./components/App";
-import {BrowserRouter} from 'react-router-dom';e
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
 
 ReactDOM.render(
   <React.StrictMode>
-<BrowserRouter>
     <App />
-</BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
